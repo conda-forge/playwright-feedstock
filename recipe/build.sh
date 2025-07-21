a3c06ea92c95d46f60d8be42981a323b21b4de80
Izer Onadim <izer.onadim@quantco.com>
2025-07-21 15:42:56 +0100

    Add code for generating TPL file

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,3 +16,12 @@ NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 # install playwright globally from the npm registry
 npm install -g ${PKG_NAME}@${PKG_VERSION}
+
+# All things after this are just concerned with generating the ThirdPartyLicenses.txt file.
+
+# Let us use pnpm for licenses
+cat <<< $(jq 'del(.packageManager)' package.json) > package.json
+
+pnpm import
+pnpm install --prod
+pnpm licenses list --json --prod | pnpm-licenses generate-disclaimer --json-input --output-file=ThirdPartyLicenses.txt
